b6fc1ef4581ab4d6b74a4dda63633f249ab2ff6e
lataba <ltballeste@gmail.com>
2023-05-12 15:39:05 -0700

    fixing the exit with the command line contains a -

--- monty_func.c
+++ monty_func.c
@@ -11,7 +11,7 @@ void push(stack_t **stack, unsigned int line_number)
 	stack_t *new_node = NULL;
 	int n = 0;
 
-	if (!tok || !is_number(tok))
+	if (!tok || !is_number(tok) || *tok == '-')
 	{
 		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
 		exit_free(stack, file_opcode.opcode, file_opcode.file);
