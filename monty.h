0c23240e880396a62de36d3e75b7c4d743783d51
root <gonzalopeder2@gmail.com>
2023-05-12 14:47:36 -0700

    header update with new prototypes

--- monty.h
+++ monty.h
@@ -1,5 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -7,14 +16,15 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -25,7 +35,33 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct GlobalVariables - The global variables to be free
+ * @file: Descriptor of the Monty file
+ * @opcode: The command to take from the Monty file
+ */
+struct GlobalVariables
+{
+	FILE *file;
+	char *opcode;
+} file_opcode;
+
+
+/* Monty functions prototypes*/
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+/* Auxiliar functions prototypes*/
+void opcomp(stack_t **stack, unsigned int line_number, char *opcode);
+bool is_number(const char *str);
+void exit_free(stack_t **stack, char *line, FILE *file);
+void free_stack(stack_t *stack);
+
 #endif
