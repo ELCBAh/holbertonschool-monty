59a3f7be778c9117a48eeffa7eb2bda28bab4596
gonzalopedernera <gonzalopeder2@gmail.com>
2023-05-12 19:36:04 -0700

    Error output correction

--- add.c
+++ add.c
@@ -17,7 +17,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (tmp->next == NULL) /*checks if there are at least 2  elements*/
 	{
-		dprintf(STDERR_FILENO, "L<%d>: can't add, stack too short\n", line_number);
+		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
