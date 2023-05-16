3b466b0cbf6c3ab741667a31ae02e1dee586a04e
lataba <ltballeste@gmail.com>
2023-05-16 07:51:08 -0700

    fixing betty pop sub

--- pop.c
+++ pop.c
@@ -1,26 +1,30 @@
 #include "monty.h"
+
 /**
  * pop - initializes prototype
- * 
+ *
  * @stack: the stack to modify
- * 
+ *
  * @line_number: number of line where the opcode is
- * 
+ *
  * Description: removes the top element of the stack
-*/
+ */
+
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp;
+	stack_t *tmp;
 
-    /* Checking if top element of the stack is empty or NULL (checking allocation and content) */
-    if (!stack || !*stack)
-    {
-        dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    tmp = *stack; /* Holding top element of the stack */
-    *stack = (*stack)->next;
-    free(tmp);
-    if (*stack)
-        (*stack)->prev = NULL;
+	/* Checking if top element of the stack is empty*/
+	/* or NULL (checking allocation and content) */
+	if (!stack || !*stack)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *stack; /* Holding top element of the stack */
+	*stack = (*stack)->next;
+	free(tmp);
+	if (*stack)
+		(*stack)->prev = NULL;
 }
--- sub.c
+++ sub.c
@@ -1,29 +1,32 @@
 #include "monty.h"
+
 /**
  * sub - initializes prototype
- * 
+ *
  * @stack: the stack to modify
- * 
+ *
  * @line_number: number of the line to find the opcode
- * 
- * Description: subtracts the top element of the stack from the second top element
- * 
-*/
+ *
+ * Description: subtracts the top element of the stack
+ *
+ */
+
 void sub(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp;
+	stack_t *tmp;
 
-    /** Checking stack, allocation, empty and enough elements */
-    if (!stack || !*stack || !(*stack)->next)
-    {
-        dprintf(STDERR_FILENO, "L%d: can't sub, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    tmp = *stack;
-    tmp = tmp->next; /* Saving stack and moving to next node */
-    tmp->n -= (*stack)->n;
-    tmp->prev = NULL; /* Substracting elements and removing previous node*/
-    free(*stack);
-    *stack = tmp;
-    (*stack)->next = tmp->next;
+	/* Checking stack, allocation, empty and enough elements */
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *stack;
+	tmp = tmp->next; /* Saving stack and moving to next node */
+	tmp->n -= (*stack)->n;
+	tmp->prev = NULL; /* Substracting elements and removing previous node*/
+	free(*stack);
+	*stack = tmp;
+	(*stack)->next = tmp->next;
 }
